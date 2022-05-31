7383a584efb9df854c54c7ee56206d079aeee369
Cese424 <103143331+Cese424@users.noreply.github.com>
2022-05-31 08:37:08 -0300

    school test 1

---
+++ school-test/Tile.js
@@ -0,0 +1,68 @@
+export default class Tile {
+
+  #tileElement
+  #x
+  #y
+  #value
+
+  constructor(tileContainer, value = Math.random() > 0.2 ? 2 : 4) {
+    this.#tileElement = document.createElement("div")
+    this.#tileElement.classList.add("tile")
+    tileContainer.append(this.#tileElement)
+    this.value = value
+  }
+
+  get value() {
+    return this.#value
+  }
+
+  set value(v) {
+    this.#value = v
+    this.#tileElement.textContent = v
+    const power = Math.log2(v)
+    const backgroundLightness = 100 - power * 9
+    this.#tileElement.style.setProperty(
+      "--background-lightness",
+      `${backgroundLightness}%`
+    )
+    this.#tileElement.style.setProperty(
+      "--text-lightness",
+      `${backgroundLightness <= 50 ? 90 : 10}%`
+    )
+    if (v >= 100000) {
+      this.#tileElement.style.fontSize = "1,25vw"
+    }
+    else if (v >= 10000) {
+      this.#tileElement.style.fontSize = "1.75vw"
+    }
+    else if (v >= 1000) {
+      this.#tileElement.style.fontSize = "2.25vw"
+    }
+  }
+
+  set x(value) {
+    this.#x = value
+    this.#tileElement.style.setProperty("--x", value)
+  }
+
+  set y(value) {
+    this.#y = value
+    this.#tileElement.style.setProperty("--y", value)
+  }
+
+  remove() {
+    this.#tileElement.remove()
+  }
+
+  waitForTransition(animation = false) {
+    return new Promise(resolve => {
+      this.#tileElement.addEventListener(
+        animation ? "animationend" : "transitionend",
+        resolve,
+        {
+          once: true,
+        }
+      )
+    })
+  }
+}
